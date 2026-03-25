6b77746c755f1e0349eb4d7c11c74a28b184a4dc
unknown <lynnabbidi@gmail.com>
2026-03-25 20:47:02 +0400

    uh yipee other button work

--- src/app/pages/add-task/add-task.page.ts
+++ src/app/pages/add-task/add-task.page.ts
@@ -4,6 +4,8 @@ import { IonicModule, NavController } from "@ionic/angular";
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Task } from '../../models/task.model';
+import { AlertController } from '@ionic/angular';
+import { IONIC_ICONS } from '../../../assets/data/icon';
 
 @Component({
   selector: 'app-add-task',
@@ -12,6 +14,9 @@ import { Task } from '../../models/task.model';
   imports: [IonicModule, CommonModule, FormsModule],
 })
 export class AddTaskPage implements OnInit {
+  isEditingCustom = false;
+  allIcons = IONIC_ICONS;
+  filteredIcons = [...IONIC_ICONS];
 
   minDate: string = new Date().toISOString();
 
@@ -34,14 +39,9 @@ export class AddTaskPage implements OnInit {
     { name: 'Chore', icon: 'home-outline' }
   ];
 
-  constructor(private navCtrl: NavController) { }
+  constructor(private navCtrl: NavController, private alertCtrl: AlertController) { }
 
   ngOnInit() {
-  }
-
-selectCategory(cat: any) {
-    this.newTask.type = cat.name;
-    this.newTask.icon = cat.icon;
   }
 
   startVoiceRecognition() {
@@ -52,6 +52,46 @@ selectCategory(cat: any) {
   saveTask() {
     console.log("Saving to Firebase/Local...", this.newTask);
     // Logic to push to array and navigate back
+  }
+
+  filterIcons(event: any) {
+    const query = event.target.value.toLowerCase();
+    this.filteredIcons = this.allIcons.filter(icon =>
+      icon.name.toLowerCase().includes(query)
+    );
+  }
+
+  startCustomEdit() {
+    this.isEditingCustom = true;
+    // If starting fresh, set a default icon
+    if (!this.isCustomCategorySelected()) {
+      this.newTask.icon = 'apps-outline';
+      this.newTask.type = '';
+    }
+  }
+
+  selectCategory(cat: any) {
+    this.newTask.type = cat.name;
+    this.newTask.icon = cat.icon;
+    this.isEditingCustom = false; // Hide the setup card if a preset is picked
+  }
+
+  selectCustomIcon(icon: any, modal: any) {
+    this.newTask.icon = icon.value;
+    modal.dismiss();
+  }
+
+  isCustomCategorySelected() {
+    return this.newTask.type !== '' && !this.categories.some(cat => cat.name === this.newTask.type);
+  }
+
+  toggleCustomEdit() {
+    this.isEditingCustom = !this.isEditingCustom;
+    // Set some defaults if they are opening it for the first time
+    if (this.isEditingCustom && !this.isCustomCategorySelected()) {
+      this.newTask.type = '';
+      this.newTask.icon = 'help-circle-outline';
+    }
   }
 }
 
